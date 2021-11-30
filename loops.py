3f1cfd86f00d158cf0b494a2e38af3aef87a890a
Jomar A. Nacorda <75926407+Jomar77@users.noreply.github.com>
2021-11-30 11:50:08 +0800

    Added comments

--- loops.py
+++ loops.py
@@ -1,9 +1,10 @@
 import sys
 k = []
-for i in range(sys.maxsize):                    # I couldn't figure out how to get an indefinite loop out of a for loop so I did this
+for i in range(sys.maxsize):                                # I couldn't figure out how to get an indefinite loop out of a for loop so I did this
     n = input("Enter a number or press Enter to quit: ")
-    k += [n]
-    if k[i] == "":
+    k += [n]                                                # puts in value in the array
+    if k[i] == "":                                          # If the input is empty, then the loop breaks
         break
-    arr = [int(x) for x in k]
+    arr = [int(x) for x in k]                               # Conversion of the string array to int array
+    
 print("The sum is ", sum(arr), "\nThe average is", sum(arr)/len(arr))
