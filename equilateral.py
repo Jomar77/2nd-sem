e28dbb3b48cb70032cd2dc6c4753595444342ba8
JOMAR NACORDA <jnacorda04@gmail.com>
2021-11-30 10:15:32 +0800

    Exercise 5

---
+++ equilateral.py
@@ -0,0 +1,11 @@
+
+
+j = int(input("Enter the first side:"))
+k = int(input("Enter the second side:"))
+l = int(input("Enter the third side:"))
+
+
+if j  != k or k != l:
+    print("The triangle is not equilateral")
+else:
+    print("The triangle is equilateral")
