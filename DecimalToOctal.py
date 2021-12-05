8e1a319c9db8abfd5b60a146bc38fb248c2f6764
JOMAR NACORDA <jnacorda04@gmail.com>
2021-12-05 16:09:07 +0800

    change varname

--- DecimalToOctal.py
+++ DecimalToOctal.py
@@ -3,12 +3,12 @@ if  decimal  == 0:
     print(0) 
 else: 
     print("Quotient Remainder Octal")
-bitString  = "   "
+    octString  = "   "
 
 while  decimal  >  0:
     remainder  =  decimal  %  8 
     decimal  = decimal  //  8 
-    bitString  =  str(remainder)  + bitString
-    print("%5d%8d%12s"  %  (decimal, remainder, bitString))
-    
-print(" The Octal representation is ",  bitString)
+    octString  =  str(remainder)  + octString
+    print("%5d%8d%12s"  %  (decimal, remainder, octString))
+
+print(" The Octal representation is ",  octString)
