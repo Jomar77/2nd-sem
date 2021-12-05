a870b3ffa38dcfc70f831045ba34a394c96212a5
JOMAR NACORDA <jnacorda04@gmail.com>
2021-12-05 15:58:17 +0800

    remove print

--- OctToDecimal.py
+++ OctToDecimal.py
@@ -1,7 +1,6 @@
 octString  = input("Enter a  string  of  octal digits: ")
 decimal  = 0
 exponent  =  len(octString) - 1
-print(exponent)
 for  digit  in  octString: 
     decimal  = decimal  + int(digit)  *  8  **  exponent 
     exponent  =  exponent  - 1 
