81d1e9b4ef6ca014f8290b21595c6e484d74e360
JOMAR NACORDA <jnacorda04@gmail.com>
2021-12-09 14:18:45 +0800

    udpates

---
+++ vennDiagram.py
@@ -0,0 +1,8 @@
+B = [1,2,3]
+A = [1,2,3,4,5,6,7]
+
+print("The cardinality of a: ",len(A), "\nThe cardinality of b: ", len(B))
+print("B is a subset of A:", set(B) <= set(A))
+print("B is a subset of A:", set(B) < set(A))
+#Jomar A. Nacorda
+
