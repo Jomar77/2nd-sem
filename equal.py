020d71258717bf0a723eb89279d39b9742ed6438
JOMAR NACORDA <jnacorda04@gmail.com>
2021-12-09 17:48:09 +0800

    update

---
+++ equal.py
@@ -0,0 +1,7 @@
+a = [1,3,5,8,9]
+b = [1,3,5,7,9]
+
+print("Cardinality of A: ",	len(a))
+print("Cardinality of B: ",	len(b))
+print("Equal set:  ", a==b)
+print("Equivalent set:  ", (len(a) == len(b))) 
