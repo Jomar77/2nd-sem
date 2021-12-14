3107ced66451808e3816c319d3be29c8a7fcf36f
JOMAR NACORDA <jnacorda04@gmail.com>
2021-12-14 16:21:22 +0800

    week 3 exercise

---
+++ java/equivalent.java
@@ -0,0 +1,145 @@
+
+public class equivalent{
+    public static void main(String args[])
+    {
+        int a[] = { 1,1,2,3,4,6,8,9};
+        int b[] = { 2,3,3,4,5,6,7 };
+        equivalent eq =new equivalent();
+
+        eq.union(a, b);
+        eq.unionAll(a, b);
+        eq.intersect(a, b);
+        eq.diff(a, b);
+        eq.symDiff(a, b);
+    }
+
+
+    public void union(int a[], int b[]){
+        String hold = "";
+
+        for(int i = 0; i < a.length; i++){
+            hold += a[i]+" ";
+        }
+        for(int i = 0; i < b.length; i++){
+            hold += b[i]+" ";
+        }
+
+        String s = "";
+
+        for (int i = 0; i < hold.length(); i++)
+        {
+
+            char val = hold.charAt(i);
+
+            if (s.indexOf(val) < 0)
+            {
+                s += val + " ";
+            }
+        }
+        System.out.println(s);
+   }
+
+
+    public void unionAll(int a[], int b[]){
+        String hold = "";
+        for(int i = 0; i < a.length; i++){
+            hold += a[i]+" ";
+        }
+        for(int i = 0; i < b.length; i++){
+            hold += b[i]+" ";
+        }
+
+        System.out.println(hold);
+    }
+
+    public void intersect(int a[], int b[]){
+        String hold = "";
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++)
+                if (a[i] == b[j]){
+                   hold += b[j] + " ";
+                }
+            }
+            String s = "";
+
+            for (int i = 0; i < hold.length(); i++)
+            {
+                char val = hold.charAt(i);
+                if (s.indexOf(val) < 0)
+                {
+                    s += val + " ";
+                }
+            }
+            System.out.println(s);
+        }
+
+        public void diff(int a[], int b[]){
+
+            String hold = "";
+
+            for(int i = 0; i < a.length; i++){
+                hold += a[i]+" ";
+            }
+
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < b.length; j++){
+                    if (a[i] == b[j]){
+
+                        hold =  hold.replace(Integer.toString(a[i]), "");
+                        break;
+                    }
+                }
+            }
+
+            String s = "";
+
+            for (int i = 0; i < hold.length(); i++)
+            {
+                char val = hold.charAt(i);
+                if (s.indexOf(val) < 0)
+                {
+                    s += val + " ";
+                }
+            }
+            System.out.println(s);
+        }
+
+
+
+        public void symDiff(int a[], int b[]){
+
+            String hold = "";
+
+            for(int i = 0; i < a.length; i++){
+                hold += a[i]+" ";
+            }
+
+
+            for(int i = 0; i < b.length; i++){
+                hold += b[i]+" ";
+            }
+
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < b.length; j++){
+                    if (a[i] == b[j]){
+
+                        hold =  hold.replace(Integer.toString(a[i]), "");
+                        break;
+                    }
+                }
+            }
+
+            String s = "";
+
+            for (int i = 0; i < hold.length(); i++)
+            {
+                char val = hold.charAt(i);
+                if (s.indexOf(val) < 0)
+                {
+                    s += val + " ";
+                }
+            }
+            System.out.println(s);
+        }
+
+}
